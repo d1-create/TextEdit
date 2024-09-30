970668f4d5fb4113db3946bb907d65062fe0b956
Dirushan <116972461+d1-create@users.noreply.github.com>
2024-09-30 21:01:36 +0100

    startnewproj

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+import ui.maincontainer;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        maincontainer maincontainer_obj = new maincontainer();
+        maincontainer_obj.setVisible(true);
+    }
+}
